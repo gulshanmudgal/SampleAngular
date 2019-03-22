53cfbd50dbd0329e92164dd70bbc2bd4026810bf
Gulshan Kumar <inbox.gulshan@outlook.com>
2019-03-22 10:00:17 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebarr/sidebar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactDetailsComponent } from './components/contact-details/contact-details.component';
+import { ContactListComponent } from './components/contact-list/contact-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    HomeComponent,
+    ContactDetailsComponent,
+    ContactListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/model/contact.ts
@@ -0,0 +1,12 @@
+export class Contact {
+    id: number;
+    firstName: string;
+    lastName: string;
+    gender: string = 'Male';
+    email: string;
+    phone: string;
+    dob:string;
+    picture:'/assets/images/profile.png';
+    city: string;
+    country: string;
+}
